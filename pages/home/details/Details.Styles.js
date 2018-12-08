d60fa12422dfdedfa267644f2f99299f082fa87f
thessler27 <hessler.thomas93@gmail.com>
2018-12-08 12:56:10 -0500

    stickay growth headah

--- pages/home/details/Details.Styles.js
+++ pages/home/details/Details.Styles.js
@@ -1,6 +1,51 @@
 import { StyleSheet } from 'react-native';
 
+const HEADER_MAX_HEIGHT = 200;
+const HEADER_MIN_HEIGHT = 60;
+const HEADER_SCROLL_DISTANCE = 140;
+
 export const DetailsStyles = StyleSheet.create({
 	ContainerStyles: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-	TextStyles: { color: 'green' }
+	TextStyles: { color: 'green' },
+	fill: {
+    flex: 1,
+  },
+  row: {
+    height: 40,
+    margin: 16,
+    backgroundColor: '#D3D3D3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+	  position: 'absolute',
+	  top: 0,
+	  left: 0,
+	  right: 0,
+	  backgroundColor: '#fff',
+	  overflow: 'hidden',
+	},
+	bar: {
+	  marginTop: 28,
+	  height: 32,
+	  alignItems: 'center',
+	  justifyContent: 'center',
+	},
+	title: {
+	  backgroundColor: 'transparent',
+	  color: 'white',
+	  fontSize: 18,
+	},
+	scrollViewContent: {
+	  marginTop: HEADER_MAX_HEIGHT,
+	},
+	backgroundImage: {
+	  position: 'absolute',
+	  top: 0,
+	  left: 0,
+	  right: 0,
+	  width: null,
+	  height: HEADER_MAX_HEIGHT,
+	  resizeMode: 'cover',
+	}
 });
--- pages/home/details/Details.js
+++ pages/home/details/Details.js
@@ -3,16 +3,25 @@ import {
     Text, 
     View, 
     Button,
-    Alert
+    Alert,
+    ScrollView,
+    Animated
 } from 'react-native';
 import { DetailsStyles } from './Details.Styles';
+
+const HEADER_MAX_HEIGHT = 200;
+const HEADER_MIN_HEIGHT = 60;
+const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
 
 export class DetailsScreen extends React.Component {
 
   constructor(props) {
     super(props);
     let team = this.props.navigation.getParam('team', {name: 'No Team'});
-    this.state = {team: team};
+    this.state = {
+      team: team,
+      scrollY: new Animated.Value(0)
+    };
   }
 
   static navigationOptions = ({ navigation }) => {
@@ -22,10 +31,82 @@ export class DetailsScreen extends React.Component {
     };
   };
 
-  render() {
+  componentDidMount () {
+    this.props.navigation.setParams({
+      scrollToTop: () => {
+        this.flatListElem.scrollToOffset({ x: 0, y: 0, animated: true })
+      }
+    });
+  }
+
+  _renderScrollViewContent() {
+    const data = Array.from({length: 30});
     return (
-      <View style={DetailsStyles.ContainerStyles}>
-        <Text style={DetailsStyles.TextStyles}>{this.state.team ? this.state.team.name : 'No Team Name'}</Text>
+      <View style={DetailsStyles.scrollViewContent}>
+        {data.map((_, i) =>
+          <View key={i} style={DetailsStyles.row}>
+            <Text>{i+1}</Text>
+          </View>
+        )}
+      </View>
+    );
+  }
+
+  render() {
+
+    const headerHeight = this.state.scrollY.interpolate({
+      inputRange: [-(HEADER_SCROLL_DISTANCE), 0, HEADER_SCROLL_DISTANCE],
+      outputRange: [HEADER_MAX_HEIGHT+HEADER_SCROLL_DISTANCE, HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+      extrapolate: 'clamp',
+    });
+
+    const imageTranslate = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE],
+      outputRange: [0, -50],
+      extrapolate: 'clamp',
+    });
+
+    const imageScale = this.state.scrollY.interpolate({
+      inputRange: [-50, 0, HEADER_SCROLL_DISTANCE],
+      outputRange: [1.125, 1, 1],
+      // extrapolate: 'clamp'
+    });
+
+    return (
+      <View style={DetailsStyles.fill}>
+        <ScrollView
+          style={DetailsStyles.fill}
+          scrollEventThrottle={16}
+          showsVerticalScrollIndicator={false}
+          onScroll={Animated.event(
+            [{
+              nativeEvent: {
+                contentOffset: { y: this.state.scrollY }
+              }
+            }]
+          )}
+        >
+          {this._renderScrollViewContent()}
+        </ScrollView>
+        <Animated.View style={
+            [
+              DetailsStyles.header, 
+              {height: headerHeight}
+            ]}>
+          <Animated.Image
+            style={[
+              DetailsStyles.backgroundImage,
+              {transform: [
+                {translateY: imageTranslate}, 
+                {scaleY: imageScale},
+                {scaleX: imageScale}
+              ]},
+            ]}
+            source={require('./img/sabres.png')}
+          />
+          <View style={DetailsStyles.bar}>
+          </View>
+        </Animated.View>
       </View>
     );
   }
